62349d7d296e59decdf9374bf5cb362fd4621e29
Austin Martinovich <martino569@yahoo.com>
2015-01-09 19:02:39 -0600

    Committing .ino of driver operations and interfacing, as well as a .cpp file to test the operations taking keyboard input.

---
+++ Interfacing/drivers/drivers.ino
@@ -0,0 +1,166 @@
+#include <Servo.h>
+
+//create the 6 servos
+Servo botPinch;
+Servo botTwist;
+Servo leftPinch;
+Servo leftTwist;
+Servo rightPinch;
+Servo rightTwist;
+
+//assign angles for operations (TBD)
+int openDeg;
+int closeDeg;
+int cwise;
+int cCwise;
+//variable to store serial input
+int operation = -1;
+
+//attach servos to PWM pins and setup serial in
+void setup()
+{
+  botPinch.attach(3);
+  botTwist.attach(5);
+  leftPinch.attach(6);
+  leftTwist.attach(9);
+  rightPinch.attach(10);
+  rightTwist.attach(11);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  //once there is input..
+  if (Serial.available() > 0)
+  {
+    //store input
+    operation = Serial.read(); 
+    //perform corresponding operation
+    switch(state)
+    {
+      case '0':
+        left();
+        break;
+      case '1':
+        right();
+        break;
+      case '2':
+        up();
+        break;
+      case '3':
+        down();
+        break; 
+      case '4':
+        front();
+        break;
+      case '5':
+        back();
+        break;  
+      default:
+        break; 
+    }
+  }
+}
+
+//every open has a close, every cwise has a cCwise
+//this ensures we maintain our home position
+//by Rubik notation, each operation is a clockwise turn
+
+// 'L' operation
+void left()
+{
+  botPinch.write(openDeg);
+  leftTwist.write(cwise);
+  botPinch.write(closeDeg);
+  leftPinch.write(openDeg);
+  leftTwist.write(cCwise);
+  leftPinch.write(closeDeg);
+}
+
+// 'R' operation
+void right()
+{
+  botPinch.write(openDeg);
+  rightTwist.write(cwise);
+  botPinch.write(closeDeg);
+  rightPinch.write(openDeg);
+  rightTwist.write(cCwise);
+  rightPinch.write(closeDeg);
+} 
+
+// 'U' operation
+void up()
+{
+  top2bot();
+  down();
+  top2bot();
+} 
+
+// 'D' operation
+void down()
+{
+  botTwist.write(cwise);
+  botPinch.write(openDeg);
+  botTwist.write(cCwise);
+  botPinch.write(closeDeg);
+}
+ 
+// 'F' operation
+void front()
+{
+  front2bot();
+  down();
+  bot2front();
+}
+
+// 'B' operation
+void back()
+{
+  bot2front();
+  down();
+  front2bot();
+}
+
+//rotate front face to bottom (for F and B)
+void front2bot()
+{
+  botPinch.write(openDeg);
+  rightPinch.write(openDeg);
+  leftTwist.write(cwise);
+  rightPinch.write(closeDeg);
+  leftPinch.write(openDeg);
+  leftTwist.write(cCwise);
+  leftPinch.write(closeDeg);
+  botPinch.write(closeDeg);
+}
+
+//rotate bottom face to front (for F and B)
+void bot2front()
+{
+  botPinch.write(openDeg);
+  rightPinch.write(openDeg);
+  leftTwist.write(cCwise);
+  rightPinch.write(closeDeg);
+  leftPinch.write(openDeg);
+  leftTwist.write(cwise);
+  leftPinch.write(closeDeg);
+  botPinch.write(closeDeg);
+}  
+
+//rotate cube so top face is on bottom (for U)
+//4 lines less than front2bot twice
+void top2bot()
+{
+  botPinch.write(openDeg);
+  rightPinch.write(openDeg);
+  leftTwist.write(cwise);
+  rightPinch.write(closeDeg);
+  leftPinch.write(openDeg);
+  leftTwist.write(cCwise);
+  rightTwist.write(cCwise);
+  leftPinch.write(closeDeg);
+  rightPinch.write(openDeg);
+  rightTwist.write(cwise);
+  rightPinch.write(closeDeg);
+  botPinch.write(closeDeg);
+}  
---
+++ Interfacing/testDrivers.cpp
@@ -0,0 +1,65 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::IO::Ports;
+
+int main(array<System::String ^> ^args)
+{
+
+	String^ answer;
+	String^ portName;
+	int baudRate = 9600;
+	Console::WriteLine("Type in a port name and hit ENTER");
+	portName = Console::ReadLine();
+	// arduino settings
+	SerialPort^ arduino;
+	arduino = gcnew SerialPort(portName, baudRate);
+	// open port
+	try
+	{
+		arduino->Open();
+
+		do
+		{
+			// ask on or off
+			Console::WriteLine("Type \"L\" \"R\" \"U\" \"D\" \"F\" or \"B\" to test the operation");
+			// get answer
+			answer = Console::ReadLine();
+			//check that user typed one of the options
+			if (String::Compare(answer, "L") == 0)
+				arduino->WriteLine("0"); // send 0 to arduino
+			else if (String::Compare(answer, "R") == 0)
+				arduino->WriteLine("1"); // send 1 to arduino
+			else if (String::Compare(answer, "U") == 0)
+				arduino->WriteLine("2"); // send 2 to arduino
+			else if (String::Compare(answer, "D") == 0)
+				arduino->WriteLine("3"); // send 3 to arduino
+			else if (String::Compare(answer, "F") == 0)
+				arduino->WriteLine("4"); // send 4 to arduino
+			else if (String::Compare(answer, "B") == 0)
+				arduino->WriteLine("5"); // send 5 to arduino
+			else
+				Console::WriteLine(answer + " was not an option");
+			// ask user if he wants to continue
+			Console::WriteLine("Try again? yes/no");
+			// get answer
+			answer = Console::ReadLine();
+			// clear the screen
+			Console::Clear();
+		} while (String::Compare(answer, "yes") == 0);
+		// close port to arduino
+		arduino->Close();
+	}
+	catch (IO::IOException^ e)
+	{
+		Console::WriteLine(e->GetType()->Name + ": Port is not ready");
+	}
+	catch (ArgumentException^ e)
+	{
+		Console::WriteLine(e->GetType()->Name + ": incorrect port name syntax, must start with COM/com");
+	}
+	// end program
+	Console::Write("Press enter to close the program");
+	Console::Read();
+	return 0;
+}
